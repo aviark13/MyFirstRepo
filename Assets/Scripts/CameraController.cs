a117ad9c0ba732dbbc945a77e623ce83d2f5b723
DESKTOP-NODSEL1\aviar <aviark13@gmail.com>
2017-11-23 00:21:08 +1300

    no message

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public float lookSmooth = 0.05f;
+	public Vector3 offsetFromTargert = new Vector3 (0, 2.5f, -8);
+	public float xTilt = 10;
+
+	private Vector3 destination = Vector3.zero;
+	private CharacterController charController;
+	float rotateVel = 0;
+
+
+	void Start () 
+	{
+		SetCameraTarget (target);
+	}
+
+	public void SetCameraTarget(Transform trans)
+	{
+		if (trans != null) 
+		{
+			if (trans.GetComponent<CharacterController>())
+			{
+				charController = trans.GetComponent<CharacterController> ();
+			}
+			else 
+			{
+				Debug.LogError ("The cameras target needs a camera controller.");
+			}
+		} 
+		else 
+		{
+			Debug.LogError ("Your camera needs a target.");
+		}
+	}
+
+	void LateUpdate () 
+	{
+		//moving
+		MoveToTarget();
+		//rotating
+		LookAtTarget();
+	}
+
+	void MoveToTarget()
+	{
+		destination = charController.TargetRotation * offsetFromTargert;
+		destination += target.position;
+		this.transform.position = destination;
+	}
+
+	void LookAtTarget()
+	{
+		float eulerYAngle = Mathf.SmoothDampAngle (this.transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+		this.transform.rotation = Quaternion.Euler (xTilt, eulerYAngle, 0);
+	}
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public float inputDelay = 0.1f;
+	public float forwardVel = 12f;
+	public float rotateVel = 200f;
+
+	public Animator anim;
+	//public RuntimeAnimatorController anim;
+
+	private Quaternion targetRotation;
+	private Rigidbody rBody;
+	float forwardInput, turnInput;
+
+
+	public Quaternion TargetRotation
+	{
+		get { return targetRotation; }
+	}
+
+	void Start()
+	{
+		//get animator
+		anim = this.GetComponent<Animator>();
+		//anim = this.GetComponent<Animator>().runtimeAnimatorController;
+
+		targetRotation = this.transform.rotation;
+		rBody = this.GetComponent<Rigidbody> ();
+		if (!rBody) 
+		{
+			Debug.Log ("The Character needs a ridgidbody.");
+		}
+
+		forwardInput = 0f;
+		turnInput = 0f;
+
+	}
+
+	void GetInput()
+	{
+		forwardInput = Input.GetAxis("Vertical");
+		turnInput = Input.GetAxis("Horizontal");
+	}
+
+	void Update()
+	{
+		GetInput ();
+		Turn ();
+	}
+
+	void FixedUpdate()
+	{
+		Run ();
+	}
+
+	void Run()
+	{
+		if (Mathf.Abs (forwardInput) > inputDelay) 
+		{
+			// move
+			rBody.velocity = this.transform.forward * forwardInput * forwardVel;
+			anim.SetFloat("vertical",forwardInput);
+		} 
+		else 
+		{
+			//stop moving
+			rBody.velocity = Vector3.zero;
+			anim.SetFloat("vertical", 0f);
+		}
+	}
+
+	void Turn()
+	{
+		if (Mathf.Abs (turnInput) > inputDelay) 
+		{
+			targetRotation *= Quaternion.AngleAxis (rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		}
+		this.transform.rotation = targetRotation;
+	}
+	
+}
